19318c8027a943c2ed65c259397922ef08728153
theyokohamalife <lashawn.toyoda@industrialml.com>
2021-06-14 08:47:17 +0900

    remove social head

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,11 @@
 <template>
   <v-app light>
-    <social-head />
     <nuxt />
     <v-footer>
       <span>Find a Doc Japan &copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
-
-<script>
-import SocialHead from "../components/SocialHead.vue";
-export default {
-  components: { SocialHead },
-  data() {
-    return {};
-  },
-};
-</script>
 
 <style>
 .v-data-table__mobile-row {
