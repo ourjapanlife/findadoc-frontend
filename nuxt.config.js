12638fff43f2dc15b239f6913241b023fef80ef1
theyokohamalife <lashawn.toyoda@industrialml.com>
2021-06-13 17:45:27 +0900

    add firebase services

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,30 +1,26 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - findadoc',
-    title: 'findadoc',
+    titleTemplate: "%s - findadoc",
+    title: "findadoc",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -32,16 +28,34 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
+    "@nuxtjs/vuetify"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    "@nuxtjs/firebase",
+    {
+      config: {
+        apiKey: "AIzaSyAjIhMU4Co5LvhhEoqiGqbF5R6zmsQfIZY",
+        authDomain: "findadoc-bc230.firebaseapp.com",
+        databaseURL: "https://findadoc-bc230-default-rtdb.firebaseio.com",
+        projectId: "findadoc-bc230",
+        storageBucket: "findadoc-bc230.appspot.com",
+        messagingSenderId: "871643437476",
+        appId: "1:871643437476:web:d7271d01ee125e9f7bc931",
+        measurementId: "G-SN6SHYMJKY"
+      },
+      services: {
+        auth: false,
+        firestore: true,
+        analytics: true
+      }
+    }
   ],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ["~/assets/variables.scss"],
     theme: {
       dark: true,
       themes: {
@@ -59,6 +73,5 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {}
+};
