ffc5d589ccae43b810c7bd6fc6464ebe5065776f
theyokohamalife <lashawn.toyoda@industrialml.com>
2021-06-14 15:39:09 +0900

    remove oauth

---
+++ pages/login.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <div class="d-flex flex-row justify-content-center">
+      <v-card></v-card>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async googleSignIn() {},
+  },
+};
+</script>
