ac7db09b700c1e3536ac31dce0003890b8b4d88e
theyokohamalife <lashawn.toyoda@industrialml.com>
2021-06-14 15:46:58 +0900

    login form created

--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,25 @@
 <template>
   <v-container>
-    <div class="d-flex flex-row justify-content-center">
-      <v-card></v-card>
-    </div>
+    <v-card>
+      <div class="d-flex flex-row justify-content-center">
+        <v-text-field
+          type="text"
+          v-model="email"
+          label="Email"
+          required
+        ></v-text-field>
+      </div>
+      <div class="d-flex flex-row justify-content-center">
+        <v-text-field
+          type="password"
+          v-model="password"
+          label="Password"
+          required
+        ></v-text-field>
+      </div>
+      <v-btn class="mr-4" @click="login"> submit </v-btn>
+      <v-btn @click="clear"> clear </v-btn>
+    </v-card>
   </v-container>
 </template>
 
@@ -15,7 +32,11 @@ export default {
     };
   },
   methods: {
-    async googleSignIn() {},
+    async login() {},
+    clear() {
+      this.name = "";
+      this.email = "";
+    },
   },
 };
 </script>
