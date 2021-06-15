fa6d0330d5e6f770c7fba0d9f2b3441cea2f8255
theyokohamalife <lashawn.toyoda@industrialml.com>
2021-06-16 01:29:28 +0900

    mass vaccination news

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,21 @@
     <div align="center">
       <h1>COVID-19 Vaccination Cancellation Waiting Lists in Japan</h1>
     </div>
+    <v-alert prominent type="error">
+      <v-row align="center">
+        <v-col class="grow">
+          Breaking news: Anyone 18 years or older that has a voucher can now get
+          vaccinated at the Tokyo or Osaka mass vaccination sites. 2021-06-16
+        </v-col>
+        <v-col class="shrink">
+          <v-btn
+            href="https://www.mod.go.jp/j/approach/defense/saigai/2020/covid/center.html"
+            target="_blank"
+            >Go to Registration Site</v-btn
+          >
+        </v-col>
+      </v-row>
+    </v-alert>
 
     <div>
       <cancellation-list />
