34aa34fd272453e603b608e67bc4850a111cca09
theyokohamalife <lashawn.toyoda@industrialml.com>
2021-06-16 11:39:35 +0900

    create language selector

---
+++ components/LanguageSelection.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <h1>{{ $t("hello") }}</h1>
+    <div class="language-picker">
+      <button @click="changeLanguage('en')">EN</button> |
+      <button @click="changeLanguage('ja')">JA</button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  methods: {
+    /**
+     * Called when a language button is clicked
+     * Changes the language displayed on the user's screen
+     */
+    changeLanguage(lang) {
+      // Change the i18n context variable's locale
+      // This makes it so the correct locale file is used
+      this.$i18n.locale = lang;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.language-picker {
+  padding: 2rem;
+}
+
+.lang-active {
+  font-weight: bold;
+}
+
+button:hover {
+  cursor: pointer;
+  opacity: 0.8;
+}
+</style>
