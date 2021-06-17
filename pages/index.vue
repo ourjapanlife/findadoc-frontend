cddb3f37ae074d88636ecddf9b1403315f08eaf7
Philip Ermish <philipermish@gmail.com>
2021-06-17 22:26:00 +0900

    added padding to title and alert (#45)

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,20 @@
 <template>
   <v-container>
     <div align="center">
-      <h1>{{ $t("index.title") }}</h1>
+      <h1 id="page-title">{{ $t("index.title") }}</h1>
     </div>
-    <alert />
+    <alert class="voucher-alert" />
     <div>
       <cancellation-list />
     </div>
   </v-container>
 </template>
+<style scoped>
+#page-title {
+  margin: 1em 0 1em 0;
+}
+
+.voucher-alert {
+  margin: 0.5em 0 2em 0;
+}
+</style>
