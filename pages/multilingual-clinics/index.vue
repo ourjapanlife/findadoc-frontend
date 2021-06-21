a7d166e6740d01cdf644724e86ecb5b2a431dfae
theyokohamalife <lashawn.toyoda@industrialml.com>
2021-06-21 21:23:46 +0900

    implement multilingual search page

---
+++ pages/multilingual-clinics/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-row align="center">
+      <v-col>
+        <v-select
+          :items="items"
+          label="Select a language"
+          outlined
+          filled
+        ></v-select>
+      </v-col>
+      <v-col>
+        <v-select
+          :items="items"
+          label="Select a specialty"
+          outlined
+          filled
+        ></v-select>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
