3b2839f60fa29be10e6809d4ffab307999cf4999
theyokohamalife <lashawn.toyoda@industrialml.com>
2021-06-22 10:36:28 +0900

    update alert

--- components/Alert.vue
+++ components/Alert.vue
@@ -1,19 +1,44 @@
 <template>
-  <v-alert dismissible border="left" colored-border type="info" elevation="1">
-    <v-row align="center">
-      <v-col class="grow">
-        Anyone 18 years or older that has a voucher can now get vaccinated at
-        the Tokyo or Osaka mass vaccination sites.
-      </v-col>
-      <v-col class="shrink">
-        <v-btn
-          dark
-          color="indigo darken-1"
-          href="https://www.mod.go.jp/j/approach/defense/saigai/2020/covid/center.html"
-          target="_blank"
-          >Go to Registration Site</v-btn
-        >
-      </v-col>
-    </v-row>
-  </v-alert>
+  <v-container>
+    <v-alert dismissible border="left" colored-border type="info" elevation="1">
+      <v-row>
+        <v-col class="grow">
+          Any resident of Japan with Japanese health insurance and ID card can
+          register for a first vaccine shot at Ikebukuro Nagomi Clinic without a
+          voucher.
+          <a
+            href="https://twitter.com/theyokohamalife/status/1407127524639838248?s=20"
+          >
+            More details.</a
+          >
+        </v-col>
+        <v-col class="shrink">
+          <v-btn
+            dark
+            color="indigo darken-1"
+            href="https://ikebukuro-nagomi-clinic.mdja.jp/index.php?del2=1"
+            target="_blank"
+            >Go to Nagomi Clinic Site</v-btn
+          >
+        </v-col>
+      </v-row>
+    </v-alert>
+    <v-alert dismissible border="left" colored-border type="info" elevation="1">
+      <v-row align="center">
+        <v-col class="grow">
+          Anyone 18 years or older that has a voucher can now get vaccinated at
+          the Tokyo or Osaka mass vaccination sites.
+        </v-col>
+        <v-col class="shrink">
+          <v-btn
+            dark
+            color="indigo darken-1"
+            href="https://www.mod.go.jp/j/approach/defense/saigai/2020/covid/center.html"
+            target="_blank"
+            >Go to Registration Site</v-btn
+          >
+        </v-col>
+      </v-row>
+    </v-alert>
+  </v-container>
 </template>
