cb64164451d47caf3cc3cf0cf6ff49023c464d1d
LaShawn Toyoda <31802656+theyokohamalife@users.noreply.github.com>
2021-06-24 08:41:58 +0900

    link icon with text (#144)

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -3,16 +3,16 @@
     <v-col fluid align-content="center" align="center">
       <v-row>
         <v-col id="donate">
-          <v-icon>mdi-heart</v-icon>
           <a href="https://ko-fi.com/theyokohamalife" target="_blank">
-            {{ $t("footer.donate") }}</a
-          >
+            <v-icon>mdi-heart</v-icon>
+            {{ $t("footer.donate") }}
+          </a>
         </v-col>
         <v-col id="netlify" align-content="right">
-          <v-icon>mdi-flash</v-icon>
           <a href="https://www.netlify.com/" target="_blank">
-            {{ $t("footer.netlify") }}</a
-          >
+            <v-icon>mdi-flash</v-icon>
+            {{ $t("footer.netlify") }}
+          </a>
         </v-col>
       </v-row>
     </v-col>
