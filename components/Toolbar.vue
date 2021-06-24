ae324753a30ad3fc1417988ec4e4b4a50f6977d3
Ann 杏 Kilzer <akilzer@gmail.com>
2021-06-24 11:23:15 +0900

    prettier fixes

--- components/Toolbar.vue
+++ components/Toolbar.vue
@@ -63,7 +63,11 @@ export default {
   computed: {
     items() {
       return [
-        { title: this.$t("toolbar.home"), icon: "mdi-home-city", route: "index" },
+        {
+          title: this.$t("toolbar.home"),
+          icon: "mdi-home-city",
+          route: "index",
+        },
         this.$store.getters.isUserLoggedIn
           ? [
               {
@@ -71,10 +75,22 @@ export default {
                 icon: "mdi-shield-account-variant",
                 route: "admin-pending",
               },
-              { title: this.$t("toolbar.logout"), icon: "mdi-account", route: "logout" },
+              {
+                title: this.$t("toolbar.logout"),
+                icon: "mdi-account",
+                route: "logout",
+              },
             ]
-          : { title: this.$t("toolbar.login"), icon: "mdi-account", route: "login" },
-        { title: this.$t("toolbar.about"), icon: "mdi-head-question", route: "about" },
+          : {
+              title: this.$t("toolbar.login"),
+              icon: "mdi-account",
+              route: "login",
+            },
+        {
+          title: this.$t("toolbar.about"),
+          icon: "mdi-head-question",
+          route: "about",
+        },
       ].flat();
     },
   },
