a3e2e4d15cf47c103e3b27625e262a98bf5fcd1f
Ann 杏 Kilzer <akilzer@gmail.com>
2021-06-25 15:15:00 +0900

    Fraud alert (#158)
    * fraud alert
    
    * add ermish to the troll anihilation squad
    
    * refine fraud alert
    
    * Update components/AlertsList.vue
    
    Co-authored-by: LaShawn Toyoda <31802656+theyokohamalife@users.noreply.github.com>
    
    * Update AlertsList.vue
    
    * Update AlertsList.vue
    
    Co-authored-by: LaShawn Toyoda <31802656+theyokohamalife@users.noreply.github.com>

--- components/AlertsList.vue
+++ components/AlertsList.vue
@@ -1,15 +1,6 @@
 <template>
   <v-container class="pb-sm-0 ma-sm-0">
-    <alert>
-      The only vaccines approved in Japan are Moderna and Pfizer. Be wary of
-      clinics offering SinoVac or others, or promiming referal bonuses. The
-      content on this site is user generated and while we do our best to
-      moderate it, it may contain errors. Please use proper judgement before
-      entering your personal data on any of third party sites. We are not liable
-      for inaccurate information from these community submissions. Please use
-      the report feature, or tweet to @theyokohamalife or @stonecoldkilzer if
-      you find fraud.
-    </alert>
+    
     <alert
       :buttonText="$t('index.register')"
       buttonUrl="https://www.mod.go.jp/j/approach/defense/saigai/2020/covid/center.html"
@@ -22,5 +13,19 @@
     >
       {{ $t("index.prevaccinationScreening") }}
     </alert>
+    <div id="disclaimer">
+      The only vaccines approved in Japan are Moderna and Pfizer. Be wary of
+      clinics offering SinoVac or others. The content on this site is user generated and while
+      we do our best to moderate it, it may contain errors or unverified
+      content. We are not liable for inaccurate information
+      from these community submissions, and we are not liable for third-party
+      sites. Use your best judgment and please use the report feature if you suspect fraudulent information.
+    </div>
   </v-container>
 </template>
+
+<style scoped>
+#disclaimer {
+  padding-bottom: 8px;
+}
+</style>
