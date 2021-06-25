2cdb1fa1911babdb6e8b6c201e95658f763eae8e
Derek Barncard <derek@barncard.com>
2021-06-25 22:18:21 +0900

    Localize disclaimer (#159) (#171)

--- components/Disclaimer.vue
+++ components/Disclaimer.vue
@@ -1,13 +1,6 @@
 <template>
   <div id="disclaimer">
-    The only vaccines approved in Japan are Moderna and Pfizer. Be wary of
-    clinics offering SinoVac or others. The content on this site is user
-    generated and while we do our best to moderate it, it may contain errors or
-    unverified content. <br /><br />
-    We are not liable for inaccurate information from these community
-    submissions, and we are not liable for third-party sites. Use your best
-    judgment and please use the report feature if you suspect fraudulent
-    information.
+    <p v-html="$t('message.disclaimerAlert')"></p>
   </div>
 </template>
 
