fa9eda8dc5afd4a99f8d4de8ceb10d10733af8d6
LaShawn Toyoda <31802656+theyokohamalife@users.noreply.github.com>
2021-06-25 15:07:19 +0900

    Update AlertsList.vue

--- components/AlertsList.vue
+++ components/AlertsList.vue
@@ -1,19 +1,6 @@
 <template>
   <v-container class="pb-sm-0 ma-sm-0">
-    <alert
-      buttonText="view license"
-      buttonUrl="https://github.com/ourjapanlife/findadoc-frontend/blob/main/LICENSE"
-    >
-      The only vaccines approved in Japan are Moderna and Pfizer. Be wary of
-      clinics offering SinoVac or others, promising referal bonuses, or asking
-      for advanced payment. The content on this site is user generated and while
-      we do our best to moderate it, it may contain errors or unverified
-      content. Please use proper judgement before entering your personal data on
-      any of third party sites. We are not liable for inaccurate information
-      from these community submissions, and we are not liable for third party
-      sites. Please use the report feature, or tweet to @theyokohamalife,
-      @stonecoldkilzer, or @philipermish if you find fraud or abuse.
-    </alert>
+    
     <alert
       :buttonText="$t('index.register')"
       buttonUrl="https://www.mod.go.jp/j/approach/defense/saigai/2020/covid/center.html"
@@ -26,5 +13,13 @@
     >
       {{ $t("index.prevaccinationScreening") }}
     </alert>
+    <div>
+      The only vaccines approved in Japan are Moderna and Pfizer. Be wary of
+      clinics offering SinoVac or others. The content on this site is user generated and while
+      we do our best to moderate it, it may contain errors or unverified
+      content. We are not liable for inaccurate information
+      from these community submissions, and we are not liable for third-party
+      sites. Use your best judgment and please use the report feature if you suspect fraudulent information.
+    </div>
   </v-container>
 </template>
