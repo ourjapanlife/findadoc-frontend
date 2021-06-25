6c8ff498e31b5fdcaf1d0d86f9a226c41b0a5606
Ann 杏 Kilzer <akilzer@gmail.com>
2021-06-25 14:34:52 +0900

    refine fraud alert

--- components/AlertsList.vue
+++ components/AlertsList.vue
@@ -5,13 +5,14 @@
       buttonUrl="https://github.com/ourjapanlife/findadoc-frontend/blob/main/LICENSE"
     >
       The only vaccines approved in Japan are Moderna and Pfizer. Be wary of
-      clinics offering SinoVac or others, or promiming referal bonuses. The
-      content on this site is user generated and while we do our best to
-      moderate it, it may contain errors. Please use proper judgement before
-      entering your personal data on any of third party sites. We are not liable
-      for inaccurate information from these community submissions. Please use
-      the report feature, or tweet to @theyokohamalife, @stonecoldkilzer, or
-      @philipermish if you find fraud.
+      clinics offering SinoVac or others, promiming referal bonuses, or asking
+      for advanced payment. The content on this site is user generated and while
+      we do our best to moderate it, it may contain errors or unverified
+      content. Please use proper judgement before entering your personal data on
+      any of third party sites. We are not liable for inaccurate information
+      from these community submissions, and we are not liable for third party
+      sites. Please use the report feature, or tweet to @theyokohamalife,
+      @stonecoldkilzer, or @philipermish if you find fraud or abuse.
     </alert>
     <alert
       :buttonText="$t('index.register')"
