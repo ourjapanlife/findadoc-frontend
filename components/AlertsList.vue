ffef18e15b4ee4a3921f332225a3b6a74ee7e3fa
LaShawn Toyoda <31802656+theyokohamalife@users.noreply.github.com>
2021-06-25 15:34:01 +0900

    create disclaimer component (#160)

--- components/AlertsList.vue
+++ components/AlertsList.vue
@@ -1,6 +1,5 @@
 <template>
   <v-container class="pb-sm-0 ma-sm-0">
-    
     <alert
       :buttonText="$t('index.register')"
       buttonUrl="https://www.mod.go.jp/j/approach/defense/saigai/2020/covid/center.html"
@@ -13,19 +12,5 @@
     >
       {{ $t("index.prevaccinationScreening") }}
     </alert>
-    <div id="disclaimer">
-      The only vaccines approved in Japan are Moderna and Pfizer. Be wary of
-      clinics offering SinoVac or others. The content on this site is user generated and while
-      we do our best to moderate it, it may contain errors or unverified
-      content. We are not liable for inaccurate information
-      from these community submissions, and we are not liable for third-party
-      sites. Use your best judgment and please use the report feature if you suspect fraudulent information.
-    </div>
   </v-container>
 </template>
-
-<style scoped>
-#disclaimer {
-  padding-bottom: 8px;
-}
-</style>
