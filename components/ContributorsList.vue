36e2e827c38ab70c50fecac5f42fe1981581fca1
LaShawn Toyoda <31802656+theyokohamalife@users.noreply.github.com>
2021-06-28 01:49:10 +0900

    add updated about info (#194)
    * add updated about info
    
    * update teresa
    
    * add last name
    
    * bump submodule
    
    * update team details
    
    * revert eslint
    
    * add phil twitter

--- components/ContributorsList.vue
+++ components/ContributorsList.vue
@@ -1,30 +1,29 @@
 <template>
   <v-container>
-    <v-row class="ma-2">
-      <h3>Project Leads</h3>
+    <v-row>
+      <h2>{{ $t("about.contributorLeads") }}</h2>
     </v-row>
     <v-row>
       <v-col sm="4" md="5" lg="6" v-for="lead in leads" :key="lead.name">
         <contributor-lead
-        :githubHandle="lead.githubHandle"
-        :twitterHandle="lead.twitterHandle"
-        :name="lead.name"
-        :role="lead.role"
-        :customPic="lead.customPic" />
+          :githubHandle="lead.githubHandle"
+          :twitterHandle="lead.twitterHandle"
+          :name="lead.name"
+          :role="lead.role"
+          :customPic="lead.customPic"
+        />
       </v-col>
     </v-row>
   </v-container>
 </template>
 <script>
-
 import { leads } from "../data/contributors.json";
 
 export default {
   data() {
     return {
-      leads: leads
-    }
-  }
+      leads: leads,
+    };
+  },
 };
-
 </script>
