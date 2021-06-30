d56e14a09fcb389350986caa07a564b29714fc17
Claudio Freitas <claudiofreitas3@gmail.com>
2021-06-30 23:16:37 +0900

    Add target static to nuxt config.

--- nuxt.config.js
+++ nuxt.config.js
@@ -93,6 +93,8 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
+  target: "static",
+
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
