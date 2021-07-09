4c2237d904ade0e38bc2de275f2adeb540b868ab
LaShawn <lts001@gmail.com>
2021-07-09 15:16:24 +0900

    notify open slots

--- components/AlertsList.vue
+++ components/AlertsList.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container class="pb-sm-0 ma-sm-0">
-    <alert
+    <!-- <alert
       :button-text="$t('index.register')"
       button-url="https://www.mod.go.jp/j/approach/defense/saigai/2020/covid/center.html"
     >
@@ -11,13 +11,13 @@
       button-url="https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/vaccine_tagengo.html"
     >
       {{ $t("index.prevaccinationScreening") }}
-    </alert>
+    </alert> -->
 
-    <!-- <alert
+    <alert
       :button-text="$t('message.shinYokohamaBookVaccine')"
       button-url="https://yamabikovaccine.reserve.ne.jp/sp/index.php?"
     >
       <p v-html="$t('message.shinYokohamaAlert')"></p>
-    </alert> -->
+    </alert>
   </v-container>
 </template>
