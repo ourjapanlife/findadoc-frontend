db4e095e5ec30319d3554e49c76e0178168a7940
LaShawn <lts001@gmail.com>
2021-07-20 17:50:02 +0900

    remove alert

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,11 @@
 <template>
   <div>
     <hero-section />
-    <!-- <div id="latest-news">
-      <h2 class="news-section-title">Latest from Find a Doc</h2>
-      <div v-for="article of articles" :key="article.slug">
-        <NuxtLink
-          :to="
-            localePath({ name: 'news-slug', params: { slug: article.slug } })
-          "
-        >
-          <h3 class="latest-news-title">{{ article.title }}</h3>
-        </NuxtLink>
-        <p>{{ article.description }}</p>
-      </div>
-      <div id="news-link">
-        <nuxt-link to="/news">View all news</nuxt-link>
-      </div>
-    </div>
-    <featured-news /> -->
     <v-container>
-      <alerts-list />
       <cancellation-list />
     </v-container>
   </div>
 </template>
-
-<script>
-// import FeaturedNews from "../components/FeaturedNews.vue";
-// export default {
-//   components: { FeaturedNews },
-//   async asyncData({ $content }) {
-//     const articles = await $content("articles")
-//       .only(["title", "description", "img", "slug", "author"])
-//       .sortBy("createdAt", "asc")
-//       .fetch();
-
-//     return {
-//       articles,
-//     };
-//   },
-// };
-</script>
 
 <style scoped>
 #latest-news {
