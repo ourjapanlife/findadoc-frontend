af2a27b2e45a216ff9bab9d85d35c3d64e3cf41e
LaShawn Toyoda <31802656+theyokohamalife@users.noreply.github.com>
2021-07-21 22:47:06 +0900

    Feature/featured news (#246)
    * remove line
    
    * add featured news slide and data
    
    * add mobile breakpoints
    
    * add latest news breakpoint

---
+++ components/FeaturedNews.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-carousel cycle height="400" hide-delimiter-background show-arrows-on-hover>
+    <v-carousel-item v-for="(slide, i) in slides" :key="i">
+      <v-sheet :color="slides[i].color" height="100%">
+        <v-col
+          :class="$vuetify.breakpoint.mobile ? '' : 'fill-height pt-12 px-12'"
+        >
+          <v-col>
+            <h2>Featured</h2>
+          </v-col>
+          <v-col class="text-h5">{{ slides[i].article.title }}</v-col>
+          <v-col>{{ slides[i].article.description }}</v-col>
+          <v-col>
+            <a :href="slides[i].article.link" target="_blank">
+              <v-btn color="accent" class="black--text">{{
+                slides[i].article.buttonText
+              }}</v-btn>
+            </a>
+          </v-col>
+        </v-col>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+import featuredNewsData from "../data/featured-news.json";
+export default {
+  data() {
+    return {
+      slides: [
+        { color: "#EBF8FB", article: featuredNewsData.prevaccinationScreening },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,56 +1,65 @@
 <template>
   <div>
     <hero-section />
-    <v-container>
-      <div id="news-and-featured">
-        <div class="latest-news">
-          <h2 class="news-section-title">Latest News</h2>
-          <div
-            class="article-section"
-            v-for="article of articles"
-            :key="article.slug"
-          >
-            <v-card elevation="0" outlined>
-              <v-card-title>
-                <NuxtLink
-                  :to="
-                    localePath({
-                      name: 'news-slug',
-                      params: { slug: article.slug },
-                    })
-                  "
-                >
-                  {{ article.title }}
-                </NuxtLink>
-              </v-card-title>
-              <v-card-subtitle>
-                {{ article.description }}
-              </v-card-subtitle>
-              <v-card-actions>
-                <NuxtLink
-                  :to="
-                    localePath({
-                      name: 'news-slug',
-                      params: { slug: article.slug },
-                    })
-                  "
-                >
-                  <v-btn text color="teal accent-4">
-                    Read More
-                  </v-btn></NuxtLink
-                >
-              </v-card-actions>
-            </v-card>
-          </div>
+    <div :id="$vuetify.breakpoint.mobile ? 'mobile-news' : 'desktop-news'">
+      <div :class="$vuetify.breakpoint.mobile ? '' : 'latest-news'">
+        <h2 class="news-section-title">Latest News</h2>
+        <div
+          class="article-section"
+          v-for="article of articles"
+          :key="article.slug"
+        >
+          <v-card elevation="0" outlined>
+            <v-card-title>
+              <NuxtLink
+                :to="
+                  localePath({
+                    name: 'news-slug',
+                    params: { slug: article.slug },
+                  })
+                "
+              >
+                {{ article.title }}
+              </NuxtLink>
+            </v-card-title>
+            <v-card-subtitle>
+              {{ article.description }}
+            </v-card-subtitle>
+            <v-card-actions>
+              <NuxtLink
+                :to="
+                  localePath({
+                    name: 'news-slug',
+                    params: { slug: article.slug },
+                  })
+                "
+              >
+                <v-btn text color="teal accent-4"> Read More </v-btn></NuxtLink
+              >
+            </v-card-actions>
+          </v-card>
         </div>
       </div>
+      <div
+        :class="
+          $vuetify.breakpoint.mobile
+            ? 'mobile-featured-news'
+            : 'desktop-featured-news'
+        "
+      >
+        <featured-news />
+      </div>
+    </div>
+    <v-container>
       <cancellation-list />
     </v-container>
   </div>
 </template>
 
 <script>
+import FeaturedNews from "../components/FeaturedNews.vue";
 export default {
+  components: { FeaturedNews },
   async asyncData({ $content }) {
     const articles = await $content("articles")
       .only(["title", "description", "img", "slug", "author"])
@@ -79,18 +88,24 @@ a {
 .article-section {
   margin-top: 20px;
 }
-.featured-news {
+.desktop-featured-news {
   width: 40%;
 }
-.latest-news {
-  padding-top: 40px;
-  padding-bottom: 30px;
+.mobile-featured-news {
+  margin-top: 20px;
 }
-.latest-news-title {
-  padding-top: 20px;
+.latest-news {
+  margin-left: 80px;
+  margin-top: 60px;
+}
+#desktop-news {
+  display: flex;
+  justify-content: space-between;
+}
+#mobile-news {
+  margin: 20px;
 }
 #news-and-featured {
-  width: 100%;
   display: flex;
   justify-content: space-between;
 }
