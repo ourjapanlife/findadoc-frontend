ed8d638339bb0343b1e7599f9dcb4f8fc434fefe
LaShawn <lts001@gmail.com>
2021-07-21 15:45:52 +0900

    remove featured

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,6 @@
 <template>
   <div>
     <hero-section />
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    <div id="news-and-featured">
-      <div class="latest-news">
-        <h2 class="news-section-title">Latest from Find a Doc</h2>
-        <div v-for="article of articles" :key="article.slug">
-          <v-card class="mx-auto my-4" max-width="344" outlined>
-            <v-list-item two-line>
-              <v-list-item-content>
-                <v-list-item-title class="text-h6 mb-1">
-=======
     <v-container>
       <div id="news-and-featured">
         <div class="latest-news">
@@ -32,7 +20,6 @@
                     })
                   "
                 >
->>>>>>> bd99c00 (create news card)
                   {{ article.title }}
                 </NuxtLink>
               </v-card-title>
@@ -56,31 +43,19 @@
             </v-card>
           </div>
         </div>
-        <!-- <div class="featured-news">
-        <featured-news />
-      </div> -->
       </div>
-<<<<<<< HEAD
-    </div>
->>>>>>> 270074a (create cards for news)
-    <v-container>
-=======
->>>>>>> bd99c00 (create news card)
       <cancellation-list />
     </v-container>
   </div>
 </template>
 
 <script>
-// import FeaturedNews from "../components/FeaturedNews.vue";
 export default {
-  // components: { FeaturedNews },
   async asyncData({ $content }) {
     const articles = await $content("articles")
       .only(["title", "description", "img", "slug", "author"])
       .sortBy("createdAt", "asc")
       .fetch();
-
     return {
       articles,
     };
@@ -111,17 +86,14 @@ a {
   padding-top: 40px;
   padding-bottom: 30px;
 }
-
 .latest-news-title {
   padding-top: 20px;
 }
-
 #news-and-featured {
   width: 100%;
   display: flex;
   justify-content: space-between;
 }
-
 .news-section-title {
   font-weight: 600;
 }
