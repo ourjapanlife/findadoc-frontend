4a8f48029f2953c3ea5d6af8d26ad20322b68b58
ermish <philipermish@gmail.com>
2021-08-09 22:08:23 +0900

    removing sass

--- nuxt.config.js
+++ nuxt.config.js
@@ -158,8 +158,6 @@ export default {
   // },
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ["~/assets/variables.scss"],
-    // treeShake: true,
     theme: {
       dark: false,
       light: true,
