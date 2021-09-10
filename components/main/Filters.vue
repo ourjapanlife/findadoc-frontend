39dad85b7236d489dfb395ad165f7b02db935855
LaShawn <lts001@gmail.com>
2021-09-11 00:06:18 +0900

    add language select

--- components/main/Filters.vue
+++ components/main/Filters.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="p-8">
     <div class="font-semibold">
+      {{ $t("filters.language") }}
+    </div>
+    <div class="mt-4">
+      <label class="block">
+        <select class="form-select block w-full mt-1 p-1 border rounded">
+          <option v-for="language in languages" :key="language">
+            {{ language }}
+          </option>
+        </select>
+      </label>
+    </div>
+
+    <div class="mt-8 font-semibold">
       {{ $t("filters.doctorSpecialty") }}
     </div>
-    <div class="pt-4">
+    <div class="mt-4">
       <div v-for="specialty in specialties" :key="specialty">
-        <label>
-          <input type="checkbox" class="form-checkbox text-indigo-600" />
-          <span class="ml-2">{{ specialty }}</span>
+        <label class="inline-flex items-center">
+          <input
+            type="checkbox"
+            class="form-checkbox text-indigo-600 h-5 w-5"
+          />
+          <span class="ml-2 text-gray-700">{{ specialty }}</span>
         </label>
       </div>
     </div>
@@ -18,6 +34,7 @@
 export default {
   data() {
     return {
+      languages: ["English", "Japanese", "Vietnamese"],
       specialties: [
         "All",
         "Cardiologist",
