548216bd0a32fd467c6d879a8bcc0baa4a934a0d
= <leandrotk100@gmail.com>
2021-09-27 23:17:41 -0300

    Add test for twitter service

---
+++ services/tests/tweets-mock.js
@@ -0,0 +1,62 @@
+export const tweetsMock = [
+  {
+    author_id: "1098076915833876480",
+    id: "1405834015072874497",
+    text:
+      "I’m officially half-vaccinated! I can feel the 5G flowing through my veins…\n" +
+      "\n" +
+      "Also props to @theyokohamalife for making it so simple with her vaccine cancellation list site!",
+    name: "Spencer of Tokyo",
+    username: "TDRPlans",
+    profile_image_url:
+      "https://pbs.twimg.com/profile_images/1124420921274486784/dDt5Wz5W_normal.jpg",
+    profile_image_url_original:
+      "https://pbs.twimg.com/profile_images/1124420921274486784/dDt5Wz5W.jpg",
+  },
+  {
+    author_id: "1369534885853945857",
+    id: "1405695255962288128",
+    text:
+      'Updated our article on "How to Get a COVID-19 Vaccine in Tokyo, Japan" to point readers to the fantastic site created by @theyokohamalife, which lists open waiting lists for vaccine shots. No need for a vaccine voucher!💉👏\n' +
+      "Learn more:https://t.co/v6YxzFnD5o",
+    name: "TokyoMate",
+    username: "TokyoMate_",
+    profile_image_url:
+      "https://pbs.twimg.com/profile_images/1369536032794845184/1AhEfSdw_normal.jpg",
+    profile_image_url_original:
+      "https://pbs.twimg.com/profile_images/1369536032794845184/1AhEfSdw.jpg",
+  },
+  {
+    author_id: "148159030",
+    id: "1404959521013788676",
+    text: "@theyokohamalife Thank you LaShawn! Made my appointment!",
+    name: "Heather Dobbin",
+    username: "hdobbin",
+    profile_image_url:
+      "https://pbs.twimg.com/profile_images/1361806317787095040/1jwcKeyx_normal.jpg",
+    profile_image_url_original:
+      "https://pbs.twimg.com/profile_images/1361806317787095040/1jwcKeyx.jpg",
+  },
+  {
+    author_id: "988059344968208384",
+    id: "1404738062710579203",
+    text: "omfg i’m 1/2 vaxxed is this real life???? thank you to everyone who shared info for キャンセル待ち vaccines 😭😭😭",
+    name: "𝘮𝘪𝘬𝘢",
+    username: "paprikahhh",
+    profile_image_url:
+      "https://pbs.twimg.com/profile_images/1361212519243673608/RiL9Pmx2_normal.jpg",
+    profile_image_url_original:
+      "https://pbs.twimg.com/profile_images/1361212519243673608/RiL9Pmx2.jpg",
+  },
+  {
+    author_id: "26379689",
+    id: "1403970628470800387",
+    text: "@theyokohamalife Thank you so much, @theyokohamalife !! Just got vaccinated thanks to your help! 🙇",
+    name: "Alessandro Baffa",
+    username: "alebaffa",
+    profile_image_url:
+      "https://pbs.twimg.com/profile_images/1307206011254530048/qeqcsDRU_normal.jpg",
+    profile_image_url_original:
+      "https://pbs.twimg.com/profile_images/1307206011254530048/qeqcsDRU.jpg",
+  },
+];
