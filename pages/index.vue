b69f493aa7ca5b7492587d30b51bf65afa9db825
Philip Ermish <philipermish@gmail.com>
2021-10-31 23:50:52 +0900

    Merged the News List with Featured news. (#303)
    * Merged the News List with Featured news.
    
    * css tweak

--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,17 @@
 <template>
   <div>
     <hero-section />
-    <div class="text-h5 ml-4">{{ $t("news.latest") }}</div>
-    <v-card elevation="0" v-for="article of articles" :key="article.title">
-      <v-card-title>
-        <NuxtLink
-          :to="
-            localePath({
-              name: 'news-slug',
-              params: { slug: article.slug },
-            })
-          "
-        >
-          {{ article.title }}
-        </NuxtLink>
-      </v-card-title>
-      <v-card-subtitle>
-        {{ article.description }}
-        <NuxtLink
-          :to="
-            localePath({
-              name: 'news-slug',
-              params: { slug: article.slug },
-            })
-          "
-        >
-          <v-btn text color="secondary accent-4">
-            {{ $t("general.readMore") }}
-          </v-btn>
-        </NuxtLink>
-      </v-card-subtitle>
-      <v-divider></v-divider>
-    </v-card>
-    <div class="my-5" :align="$vuetify.breakpoint.mobile ? '' : 'center'">
+    <div
+      class="mb-4"
+      :class="$vuetify.breakpoint.mobile ? 'mt-12' : 'mt-20'">
+      <featured-news />
+    </div>
+    <div class="text-center">
       <NuxtLink to="/news">
         <v-btn text>{{ $t("index.viewAllNews") }}</v-btn>
       </NuxtLink>
     </div>
-    <div
-      :class="
-        $vuetify.breakpoint.mobile
-          ? 'mobile-featured-news'
-          : 'desktop-featured-news'
-      "
-    >
-      <featured-news />
-    </div>
-    <v-container>
+    <v-container class="mt-12">
       <cancellation-list />
     </v-container>
   </div>
